7efe22525be5b014d7bb9ad85ef8be9846553c0b
Martins <queeeeenz@gmail.com>
2020-12-02 21:42:15 +0200

    Added client login test

---
+++ client/tests/unit/login.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils'
+
+describe('Login.vue', () => {
+    it('sets loading to true when clicked on login', () => {
+        // const wrapper = shallowMount(Login)
+    })
+})
