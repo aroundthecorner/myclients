a0ad310ba66dfd6a32dbb51a475d35ad4c6a6697
Martins <queeeeenz@gmail.com>
2020-12-02 23:10:27 +0200

    Working on sidebar

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -3,5 +3,46 @@
         <title>myclients - online appointment scheduling app</title>
     </teleport>
 
-    Home
+    <div class="sidebar">
+        <div class="sidebar__profile">
+            <img
+                class="sidebar__profile-pic"
+                src="/img/profile.png"
+            />
+
+            <div class="sidebar__fullname">
+                Martins Zeltins
+            </div>
+        </div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon lab la-buffer"></i>
+            <span class="sidebar__item-text">Dashboard</span>
+        </div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon lab la-buffer"></i>
+            <span class="sidebar__item-text">Calendar</span>
+        </div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon lab la-buffer"></i>
+            <span class="sidebar__item-text">Appointments</span>
+        </div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon lab la-buffer"></i>
+            <span class="sidebar__item-text">Services</span>
+        </div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon lab la-buffer"></i>
+            <span class="sidebar__item-text">Locations</span>
+        </div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon lab la-buffer"></i>
+            <span class="sidebar__item-text">Service providers</span>
+        </div>
+    </div>
 </template>
