ac9c3836b15a4b2998870c87049e3bcbcf85cea9
Martins <martinszeltins@gmail.com>
2020-12-07 18:19:28 +0200

    Sidebar icons & settings menu

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,6 +1,6 @@
 <template>
     <teleport to="head">
-        <title>myclients - online appointment scheduling app</title>
+        <title>myclients - Online Apointment Scheduling App</title>
     </teleport>
 
     <div class="sidebar">
@@ -21,28 +21,35 @@
         </div>
 
         <div class="sidebar__item">
-            <i class="sidebar__icon lab la-buffer"></i>
+            <i class="sidebar__icon las la-calendar"></i>
             <span class="sidebar__item-text">Calendar</span>
         </div>
 
         <div class="sidebar__item">
-            <i class="sidebar__icon lab la-buffer"></i>
+            <i class="sidebar__icon las la-calendar-check"></i>
             <span class="sidebar__item-text">Appointments</span>
         </div>
 
         <div class="sidebar__item">
-            <i class="sidebar__icon lab la-buffer"></i>
+            <i class="sidebar__icon lab la-elementor"></i>
             <span class="sidebar__item-text">Services</span>
         </div>
 
         <div class="sidebar__item">
-            <i class="sidebar__icon lab la-buffer"></i>
+            <i class="sidebar__icon las la-map-marker"></i>
             <span class="sidebar__item-text">Locations</span>
         </div>
 
         <div class="sidebar__item">
-            <i class="sidebar__icon lab la-buffer"></i>
+            <i class="sidebar__icon las la-user-friends"></i>
             <span class="sidebar__item-text">Service providers</span>
+        </div>
+
+        <div class="spacer"></div>
+
+        <div class="sidebar__item">
+            <i class="sidebar__icon las la-cog"></i>
+            <span class="sidebar__item-text">Settings</span>
         </div>
     </div>
 </template>
