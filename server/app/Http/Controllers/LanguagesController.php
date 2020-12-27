b246b364df38d21b0d0204fe6349e794a891604b
Martins <queeeeenz@gmail.com>
2020-12-27 20:03:15 +0200

    Refactoring

--- server/app/Http/Controllers/LanguagesController.php
+++ server/app/Http/Controllers/LanguagesController.php
@@ -13,30 +13,36 @@ class LanguagesController extends Controller
      */
     public function index()
     {
-        $language_translations = LanguageTranslation::join(
+        return cache()->remember('languages', now()->addMonth(), function() {
+            $language_translations = $this->getLanguageTranslations();
+        
+            foreach($language_translations as $index => $language_translation) {
+                foreach($language_translation as $translation) {
+                    $translations["{$translation['lang_string']}"] = $translation['lang_translation'];
+                }
+        
+                $languages["$index"] = [
+                    'lang_short' => $index,
+                    'lang_long' => $language_translation[0]['lang_long'],
+                    'translations' => $translations,
+                ];
+            }
+
+            return response()->json($languages);
+        });
+    }
+
+    /**
+     * Return a listing of all language translations
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLanguageTranslations()
+    {
+        return LanguageTranslation::join(
             'languages', 'languages.id', '=', 'language_translations.lang_id'
         )->join(
             'language_strings', 'language_strings.id', '=', 'language_translations.lang_string_id'
         )->get()->groupBy('lang_short');
-    
-        $languages = [];
-        $translations = [];
-    
-        foreach($language_translations as $index => $language_translation)
-        {
-            foreach($language_translation as $translation) {
-                $translations["{$translation['lang_string']}"] = $translation['lang_translation'];
-            }
-    
-            $languages["$index"] = [
-                'lang_short' => $index,
-                'lang_long' => $language_translation[0]['lang_long'],
-                'translations' => $translations,
-            ];
-        }
-
-        return cache()->remember('languages', now()->addMonth(), function() use ($languages) {
-            return response()->json($languages);
-        });
     }
 }
