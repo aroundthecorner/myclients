159b20dcc02abc6b5d0734acdf2bc928f84577bc
Martins <martinszeltins@gmail.com>
2021-02-05 09:35:18 +0200

    Updated todo

--- client/src/components/Datatable/Settings/Users.vue
+++ client/src/components/Datatable/Settings/Users.vue
@@ -229,6 +229,8 @@
         const result = await OrganizationUsers.get({
             organizationId: user.value.organization_id
         })
+
+        data.value.rows = result
     }
 
     const searchedRows = computed(() => {
